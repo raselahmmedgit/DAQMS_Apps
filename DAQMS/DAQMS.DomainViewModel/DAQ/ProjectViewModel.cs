36a310c36ca988fbe3f69abdc8d9bf1392c08e24
obaidulsarker <aupo37@gmail.com>
2016-05-17 23:11:46 +0600

    Update DB

---
+++ DAQMS/DAQMS.DomainViewModel/DAQ/ProjectViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using DAQMS.Domain.Models;
+using System.Runtime.Serialization;
+using DAQMS.Domain;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using System.Collections.Generic;
+
+namespace DAQMS.DomainViewModel
+{
+    public class ProjectViewModel : Project
+    {
+        public ProjectViewModel()
+        {
+            this.CompanyList = new List<SelectListItem>();
+            this.ProjectStatusList = new List<SelectListItem>();     
+        }
+
+        [Display(Name = "Company Name")]
+        [DataMember, DataColumn(true)]
+        public string CompanyName { get; set; }
+
+        [Display(Name = "Project Status")]
+        [DataMember, DataColumn(true)]
+        public string ProjectStatus { get; set; }
+
+        public IList<SelectListItem> CompanyList { get; set; }
+        public IList<SelectListItem> ProjectStatusList { get; set; }
+
+
+    }
+}
+
