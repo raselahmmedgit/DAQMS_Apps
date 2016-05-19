9645508aabc049c04d403f1ad2c0b303aabee280
Rasel Ahmmed Bappi <rasel.bappi@hotmail.com>
2016-05-19 00:43:38 -0700

    Rasel: Added and Remove some plugin

--- DAQMS/DAQMS.Web/BootStrapper.cs
+++ DAQMS/DAQMS.Web/BootStrapper.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Web.Http;
-using System.Data.Entity;
 using System.Reflection;
-using Autofac;
-using Autofac.Integration.Mvc;
 using DAQMS.Service;
 
 namespace DAQMS.Web
@@ -13,38 +10,7 @@ namespace DAQMS.Web
         public static void Run()
         {
             //log4net.Config.XmlConfigurator.Configure(new FileInfo(System.Web.HttpContext.Current.Server.MapPath("~/Web.config")));
-
-            SetIocContainer();
         }
         
-        private static void SetIocContainer()
-        {
-            try
-            {
-                //Implement Autofac
-
-                var configuration = GlobalConfiguration.Configuration;
-                var builder = new ContainerBuilder();
-
-                // Register MVC controllers using assembly scanning.
-                builder.RegisterControllers(Assembly.GetExecutingAssembly());
-                
-                // Register service
-                builder.RegisterAssemblyTypes(typeof(UserService).Assembly)
-                .Where(t => t.Name.EndsWith("Service"))
-                .AsImplementedInterfaces().InstancePerDependency();
-
-                var container = builder.Build();
-
-                //for MVC Controller Set the dependency resolver implementation.
-                var resolverMvc = new AutofacDependencyResolver(container);
-                System.Web.Mvc.DependencyResolver.SetResolver(resolverMvc);
-            }
-            catch (Exception ex)
-            {
-                throw;
-            }
-
-        }
     }
 }
